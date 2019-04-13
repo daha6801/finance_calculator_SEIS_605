678752a5b5feaee351ef7e33fc8edd9bf9251cef
Rashmi Dahal <Rashmi.Dahal1@veritas.com>
2019-04-12 22:37:55 -0500

    Binding the total amount paid and total interest paid text labels as well

--- src/FinanceCalculator.java
+++ src/FinanceCalculator.java
@@ -26,18 +26,27 @@ public class FinanceCalculator extends Application {
 	}
 
 	void setupActions() {
+		
 		//bind itemsComboBox with data
 		view.itemsComboBox.setItems(model.infoObservableList);
-
+		
+		//by default show the super prime (781-850) in the combobox
+		view.itemsComboBox.setPromptText(String.format("%s",model.infoObservableList.get(0)));
+		view.creditInfoLabel.setText("Based on your score the average rate is 3.68%(new) or 4.34%(used).");
+		
 		//attach a listener to itemComboBox to display Credit Info description
 		view.itemsComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+						
 			if (view.itemsComboBox.getSelectionModel().getSelectedIndex() >= 0 ) {
 				view.creditInfoLabel.setText(String.format("%s",newValue.creditInfo));
 			}
 		});
 		
+		//bing the tradeInDownPaymentTextField with data
+		view.tradeInDownPaymentTextField.setText("4000");
+		
 		//bind TextField with data
-		view.carPriceTextField.setText("500");
+		view.carPriceTextField.setText("20000");
 		
 		//bind the textField data with monthly payment
 		DoubleBinding monthlyPaymentBinding = new DoubleBinding() {
@@ -65,8 +74,68 @@ public class FinanceCalculator extends Application {
 						
 		};
 		
-		//bind bmiBinding to realtimeBMILable
+		//bind monthlyPaymentBinding to monthlyPaymentAmountLabel
 		view.monthlyPaymentAmountLabel.textProperty().bind(Bindings.format("%.2f", monthlyPaymentBinding));
+						
+		//bind the textField data with total amount paid
+		DoubleBinding totalAmountPaidBinding = new DoubleBinding() {
+
+			{
+				super.bind( view.carPriceTextField.textProperty(), view.tradeInDownPaymentTextField.textProperty(), view.interestRate.valueProperty(), view.noOfMonths.valueProperty() );
+			}
+					
+			@Override
+			protected double computeValue() {
+
+				String carPrice =view.carPriceTextField.getText();
+				String tradeInDownPayment = view.tradeInDownPaymentTextField.getText();
+				Double interestRatevalue = view.interestRate.getValue();
+				Integer noOfMonthsvalue = view.noOfMonths.getValue();
+						
+				if (  !carPrice.isEmpty() && !tradeInDownPayment.isEmpty() && interestRatevalue >= 0.0 ) {
+					double carPriceDouble = Double.parseDouble(carPrice);
+					double  tradeInDownPaymentDouble = Double.parseDouble(tradeInDownPayment);
+					double noOfMonthsvalueDouble = Double.valueOf(noOfMonthsvalue);
+					return carPriceDouble * tradeInDownPaymentDouble * interestRatevalue * noOfMonthsvalueDouble;
+				} else
+				return 0;
+			}
+								
+		};
+		
+		//bind totalAmountPaidBinding to totalAmountLabel
+		view.totalAmountLabel.textProperty().bind(Bindings.format("%.2f", totalAmountPaidBinding));
+
+								
+		//bind the textField data with total interest paid
+		DoubleBinding totalInterestPaidBinding = new DoubleBinding() {
+
+			{
+				super.bind( view.carPriceTextField.textProperty(), view.tradeInDownPaymentTextField.textProperty(), view.interestRate.valueProperty(), view.noOfMonths.valueProperty() );
+			}
+							
+			@Override
+			protected double computeValue() {
+
+				String carPrice =view.carPriceTextField.getText();
+				String tradeInDownPayment = view.tradeInDownPaymentTextField.getText();
+				Double interestRatevalue = view.interestRate.getValue();
+				Integer noOfMonthsvalue = view.noOfMonths.getValue();
+								
+				if (  !carPrice.isEmpty() && !tradeInDownPayment.isEmpty() && interestRatevalue >= 0.0 ) {
+					double carPriceDouble = Double.parseDouble(carPrice);
+					double  tradeInDownPaymentDouble = Double.parseDouble(tradeInDownPayment);
+					double noOfMonthsvalueDouble = Double.valueOf(noOfMonthsvalue);
+					return carPriceDouble * tradeInDownPaymentDouble * interestRatevalue * noOfMonthsvalueDouble;
+				} else
+				return 0;
+			}
+										
+		};
+				
+		//bind totalAmountPaidBinding to realtimeBMILable
+		view.totalInterestLabel.textProperty().bind(Bindings.format("%.2f", totalInterestPaidBinding));				
+			
 	}
 
 }
